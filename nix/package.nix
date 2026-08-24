533e71b073cb17fe41ddd0fdf10cf2cad8085837
EtienneLescot <etiennelescot@gmail.com>
2026-08-24 12:50:57 +0200

    fix(nix): update npmDepsHash to match package-lock.json

--- nix/package.nix
+++ nix/package.nix
@@ -51,7 +51,7 @@ buildNpmPackage {
       );
     };
 
-  npmDepsHash = "sha256-tQBojNjUAH7ok0HXoTX1My2lzxhp5oNRVFz9hi4IoC4=";
+  npmDepsHash = "sha256-LkKX1edTPHZq5nQRrbLAn11oVw36kb0smNQMmVRMEPA=";
 
   env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
 
